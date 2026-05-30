c479570289e397186ec24fe63a3adc314f3a84f4
CHUYVET <116235568+CHUYVET@users.noreply.github.com>
2026-05-30 15:53:35 -0700

    Bump cache for v0.5.6 version hotfix

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "gestion-veterinaria-static-v22";
+const CACHE_NAME = "gestion-veterinaria-static-v23";
 const ASSETS = [
   "./",
   "./index.html",
@@ -8,6 +8,7 @@ const ASSETS = [
   "./app.js",
   "./hotfix-v049.js",
   "./hotfix-visits-v056.js",
+  "./hotfix-version-v056.js",
   "./supabase-config.js",
   "./manifest.webmanifest"
 ];
